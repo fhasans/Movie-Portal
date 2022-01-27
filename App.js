2c717aa5491a703bba3636b1c3fd2c84d7110e35
hussein <fathinsatrian358@gmail.com>
2022-01-28 00:54:46 +0700

    project initalization

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default App;
